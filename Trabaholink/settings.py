6629665231bc888648422fba6b54d1efada73a5f
LaderaJA <laderajohnalbert@gmail.com>
2025-11-22 18:00:29 +0800

    Fix email configuration: Remove ANYMAIL and properly configure SendGrid API key

--- Trabaholink/settings.py
+++ Trabaholink/settings.py
@@ -183,10 +183,17 @@ LOGIN_REDIRECT_URL = 'jobs:home'
 LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
-ANYMAIL = {
-    "SENDGRID_API_KEY": os.environ.get('SENDGRID_API_KEY', '')
-}
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'trabaholinka@gmail.com')
+
+# SendGrid Configuration
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', '')
+
+# SMTP Configuration (fallback)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 # OTP Settings
 OTP_EXPIRY_MINUTES = 10  # OTP expires after 10 minutes
