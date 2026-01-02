036eccbe94704c9bbc5a0979c8b28df27bf5ff3a
LaderaJA <laderajohnalbert@gmail.com>
2026-01-02 13:28:20 +0800

    Fix migration dependency - use 0010 which exists on production

--- jobs/migrations/0013_add_translation_fields_to_historical_models.py
+++ jobs/migrations/0013_add_translation_fields_to_historical_models.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0011_contract_end_time_contract_start_time_and_more'),
+        ('jobs', '0010_alter_jobapplication_options_contract_agreed_rate_and_more'),
     ]
 
     operations = [
