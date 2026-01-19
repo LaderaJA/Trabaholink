47762aa1acd3a4d77f85bf1a568ce0cea6548c79
LaderaJA <laderajohnalbert@gmail.com>
2026-01-19 14:48:20 +0800

    Add merge migration 0026 to resolve jobs 0024 and 0025 conflict

---
+++ jobs/migrations/0026_merge_20260119_0700.py
@@ -0,0 +1,14 @@
+# Generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobs', '0024_add_time_fields_to_historical_contract'),
+        ('jobs', '0025_add_interview_workflow'),
+    ]
+
+    operations = [
+    ]
