e70db47617fcf29255e7ffe0e3818f906cf370cf
LaderaJA <laderajohnalbert@gmail.com>
2026-01-19 14:33:53 +0800

    Add merge migrations to resolve conflicts
    - users/0018: Merge 0009_add_notification_preference and 0017_add_start_end_year_to_experience
    - jobs/0024: Merge 0023_add_time_fields_to_historical_contract and 0023_merge_20260114_1943

---
+++ jobs/migrations/0024_merge_20260119_0600.py
@@ -0,0 +1,14 @@
+# Generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobs', '0023_add_time_fields_to_historical_contract'),
+        ('jobs', '0023_merge_20260114_1943'),
+    ]
+
+    operations = [
+    ]
---
+++ users/migrations/0018_merge_20260119_0600.py
@@ -0,0 +1,14 @@
+# Generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0009_add_notification_preference'),
+        ('users', '0017_add_start_end_year_to_experience'),
+    ]
+
+    operations = [
+    ]
